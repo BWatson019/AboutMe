4232f6d0d951a05fe82044183e9aff722123aca9
Brittney Watson <brittney.watson@spartans.ut.edu>
2019-05-25 00:49:34 -0400

    added program

---
+++ Test2Pt2b.py
@@ -0,0 +1,9 @@
+"""
+<Description: Part B>
+"""
+
+def checkIfPrime(numberToCheck):
+    for x in range(2, numberToCheck):
+          if (numberToCheck%x == 0): 
+            return False
+    return True
